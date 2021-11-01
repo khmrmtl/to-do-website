9efecb646e1fd9737e4f414326049724dcfae748
merk <motalkhmer@gmail.com>
2021-11-01 15:16:17 +0800

    Revert "update to postgreSQL2"
    This reverts commit 7c0da32f

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL2", "sqlite:///todo.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///todo.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 Bootstrap(app)
